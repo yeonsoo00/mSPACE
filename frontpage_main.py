6b65b7145ed2e7ca85c033a4723deab2cda71239
yeonsoo00 <yeonsoo00.chung@gmail.com>
2024-05-20 14:56:17 -0400

    plot for communicating cells updated

--- frontpage_main.py
+++ frontpage_main.py
@@ -13,8 +13,11 @@ app = dash.Dash(__name__, server=server, suppress_callback_exceptions=True)
 
 # Load data from CSV
 df = pd.read_csv('./CLARA/Dataset/Testdata/Mapped_new/csv/Mapped_new_0.08_CommunicationWithAxis_Y_details.csv')
-column_options = [{'label': ligand, 'value': ligand} for ligand in df['Ligand']]
-row_options = [{'label': receptor, 'value': receptor} for receptor in df['Receptor']]
+column_options = [{'label': ligand, 'value': ligand} for ligand in df['Ligand'].unique()]
+row_options = [{'label': receptor, 'value': receptor} for receptor in df['Receptor'].unique()]
+
+df_gene = pd.read_csv('./assets/data_09012023/geneexpression.csv')
+geneList = [{'label': gene, 'value': gene} for gene in df_gene.columns[3:]]
 
 # Define your layout
 app.layout = html.Div([
@@ -26,60 +29,61 @@ app.layout = html.Div([
         style={'display': 'inline-block', 'width': '80%', 'margin-top': '5px'}
     ),
     dcc.Graph(id='scatter-plot', style={'margin-top': '20px'}),
+    html.Label('Select Gene to mark:', style={'margin-right': '10px', 'align-self': 'center', 'font-size': '16px'}),
+    dcc.Dropdown(
+        id='Gene-dropdown',
+        options=geneList,
+        placeholder='Select Gene',
+        style={'width': '40%', 'display': 'inline-block', 'margin-top':'10px','margin-right': '0', 'align-self': 'center'}
+    ),
     html.Div([
-        html.Label(' Select Ligand and Receptor:', style={'margin-right': '10px', 'align-self': 'center', 'font-size': '16px'}),
+        html.Label('Select Ligand and Receptor to plot Communicating cells:', style={'margin-right': '15px', 'align-self': 'center', 'font-size': '16px'}),
         dcc.Dropdown(
             id='Ligand-dropdown',
             options=column_options,
-            placeholder='Select Column',
-            style={'width': '40%', 'display': 'inline-block', 'margin-right': '0', 'align-self': 'center'}
+            placeholder='Select Ligand',
+            style={'width': '35%', 'display': 'inline-block', 'margin-right': '0', 'align-self': 'center'}
         ),
         dcc.Dropdown(
             id='Receptor-dropdown',
             options=row_options,
-            placeholder='Select Row',
-            style={'width': '40%', 'display': 'inline-block', 'margin-left': '0', 'align-self': 'center'}
+            placeholder='Select Receptor',
+            style={'width': '35%', 'display': 'inline-block', 'margin-left': '0', 'align-self': 'center'}
         )
     ],  style={'display': 'flex', 'justify-content': 'flex-start', 'align-items': 'center', 'margin-top': '20px', 'margin-bottom': '20px'}),
-    html.Div(id='update-info', style={'display': 'none'}),
     html.Div(id='message-display', style={'margin-top': '20px', 'color': 'red', 'font-weight': 'bold'}),
-    html.Div(id='button-container', style={'margin-top': '50px'}),
-    html.Div(style={'position': 'absolute', 'bottom': '10px', 'right': '10px', 'width': '200px'},
+    html.Div(id='update-info', style={'display': 'none'}),
+    dcc.Graph(id='communicating-cells', style={'margin-top': '20px'}),
+    html.Div(style={'bottom': '10px', 'right': '10px', 'width': '300px', 'margin-top':'10px'},
         children=[html.Img(src='./assets/images_dir/banner_footer.png', style={'width': '100%', 'height': 'auto'})]
     )
 ])
 @app.callback(
-    [Output('scatter-plot', 'figure'),
-    Output('message-display', 'children')],
+    Output('scatter-plot', 'figure'),
     [Input('dataset-selector', 'value'),
-     Input('Ligand-dropdown', 'value'),
-     Input('Receptor-dropdown', 'value')]
+     Input('Gene-dropdown', 'value')]
 )
-def update_output(selected_dataset, ligand, receptor):
-    # Check if Ligand and Receptor are selected to refine the plot
-    if ligand and receptor:
-        fig, message = update_plot_colors(selected_dataset, ligand, receptor)
-        if message == "No pair matches":
-            return message
-        else:
-            return fig, message
+def update_output(selected_dataset, gene):
+    if gene:
+        fig = update_plot_colors(selected_dataset, gene)
+        return fig
     elif selected_dataset:
-        # Update with initial dataset selection
         return update_plot(selected_dataset)
     return go.Figure()
 
 
 @app.callback(
-    Output('button-container', 'children'),
-    [Input('update-info', 'children')]
+    [Output('communicating-cells', 'figure'),
+    Output('message-display', 'children')],
+    [Input('Ligand-dropdown', 'value'), 
+     Input('Receptor-dropdown', 'value')]
 )
-def display_buttons(info):
-    if info == "Subplots options":
-        return html.Div([
-            html.Button("Communicated cells", id='button1', n_clicks=0, style={'margin-right': '10px'}),
-            html.Button("Button 2", id='button2', n_clicks=0)
-        ], style={'display': 'flex', 'justify-content': 'center', 'margin-top': '10px'})
-    return []
+def update_network(ligand, receptor):
+    if not ligand or not receptor:
+        return go.Figure(), "" 
+    fig, msg = update_network_graph(ligand, receptor)
+
+    return fig, msg
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- helpers.py
+++ helpers.py
@@ -7,6 +7,8 @@ from PIL import Image
 import io
 from glob import glob
 import base64
+import math
+import networkx as nx
 
 # Directory where datasets are stored
 datasets_dir = './assets'
@@ -32,6 +34,7 @@ app.layout = html.Div([
 def update_plot(selected_dataset):
     if not selected_dataset:
         return go.Figure()
+    
     # Construct paths to the dataset's CSV file and background image
     csv_file_path = os.path.join(datasets_dir, selected_dataset, 'geneexpression.csv')
     background_image_path = os.path.join(datasets_dir, selected_dataset, 'background.jpg')
@@ -49,7 +52,7 @@ def update_plot(selected_dataset):
         x=df['cellx'], 
         y=df['celly'], 
         mode='markers',
-        marker=dict(size=5,color=df.get('color', 'blue')),  # Adjust the size to be smaller
+        marker=dict(size=4,color=df.get('color', 'blue')),  
         customdata=df.values,
         hovertemplate=hovertemplate
     )])
@@ -66,57 +69,44 @@ def update_plot(selected_dataset):
             source=f"data:image/jpeg;base64,{encoded_image}",
             xref="x", yref="y",
             x=0, y=0,
-            sizex=df['cellx'].max(),  # Set the image size in x-coordinate to span the x data range
-            sizey=df['celly'].max(),  # Set the image size in y-coordinate to span the y data range
-            sizing="stretch",  # Maintain the original size and aspect ratio
+            sizex=df['cellx'].max(),  
+            sizey=df['celly'].max(),  
+            sizing="stretch",  
             opacity=0.5,
             layer="below")],
         xaxis=dict(showgrid=False),
         yaxis=dict(showgrid=False,autorange='reversed'),
-        margin=dict(l=0, r=0, t=0, b=0),  # Remove margins to fill the plot area
+        margin=dict(l=0, r=0, t=0, b=0),  
     )
 
-    return fig, ""
+    return fig
 
 
-def update_plot_colors(selected_dataset, ligand, receptor):
+def update_plot_colors(selected_dataset, gene):
 
-    ccicsv = glob('./CLARA/Dataset/Testdata/Mapped_new/csv/*_CCI_details.csv')
-    df_ = pd.read_csv(ccicsv[0])
-    df_filtered = df_[(df_['Ligand'] == ligand) & (df_['Receptor'] == receptor)]
-    cell_sender_ids = df_filtered['Sender']
-    cell_receiver_ids = df_filtered['Receiver']
-
-    cellid_list = list(set(cell_sender_ids).union(set(cell_receiver_ids)))
-    if len(cellid_list) == 0:
-        return go.Figure(), "No pair matches"
-
-    # if not selected_dataset:
-    #     return go.Figure()
-    
-    # Construct paths to the dataset's CSV file and background image
+    # CSV file and background image
     csv_file_path = os.path.join(datasets_dir, selected_dataset, 'geneexpression.csv')
     background_image_path = os.path.join(datasets_dir, selected_dataset, 'background.jpg')
-    
+
     # Load scatter plot data from CSV
     df = pd.read_csv(csv_file_path)
-    
-    # Determine marker colors: cells in cellid_list are green, others are blue or from df['color'] if exists
-    default_color = df.get('color', 'blue')  # This gets the 'color' column or defaults to 'blue' if 'color' column doesn't exist
-    df['color'] = default_color  # Assume all cells initially have the default color
-    df.loc[df['cellid'].isin(cellid_list), 'color'] = 'red'  # Cells in cellid_list are colored green
+
+    df_filtered = df[df[str(gene)] > 0]
+    cellid_list = df_filtered['cellid'].to_list()
+
+    default_color = df.get('color', 'blue') 
+    df['color'] = default_color  
+    df.loc[df['cellid'].isin(cellid_list), 'color'] = 'red' 
 
     # Create hover template
-    hovertemplate = ''
-    for col in df.columns:
-        hovertemplate += f'<b>{col}</b>: %{{customdata[{df.columns.get_loc(col)}]}}<br>'
+    hovertemplate = '<b>%{customdata}</b><br>' + '<br>'.join([f'{col}: %{{customdata[{i}]}}' for i, col in enumerate(df.columns)])
 
     # Create scatter plot
     fig = go.Figure(data=[go.Scatter(
         x=df['cellx'], 
         y=df['celly'], 
         mode='markers',
-        marker=dict(size=5, color=df['color']),  # Use the 'color' column for marker colors
+        marker=dict(size=5, color=df['color']), 
         customdata=df.values,
         hovertemplate=hovertemplate
     )])
@@ -143,7 +133,91 @@ def update_plot_colors(selected_dataset, ligand, receptor):
         margin=dict(l=0, r=0, t=0, b=0)
     )
 
+    return fig
+
+def update_network_graph(ligand, receptor):
+
+    # Load datasets
+    df_comm = pd.read_csv('./CLARA/Dataset/Testdata/Mapped_new/csv/Mapped_new_0.08_CCI_details.csv')  # CCI data
+    df_cells = pd.read_csv('./assets/data_09012023/geneexpression.csv')  # gene expression data
+
+    # Filter data
+    filtered_comm = df_comm[(df_comm['Ligand'] == ligand) & (df_comm['Receptor'] == receptor)]
+    if len(filtered_comm) == 0:
+        return go.Figure(), "No communicating cells"
+
+    # Build the graph
+    G = nx.DiGraph()
+    for _, row in df_cells.iterrows():
+        G.add_node(row['cellid'], pos=(row['cellx'], row['celly']), customdata=row.values)
+
+    for _, row in filtered_comm.iterrows():
+        G.add_edge(row['Sender'], row['Receiver'])
+
+    # Prepare to plot
+    node_x = []
+    node_y = []
+    node_color = []
+    node_customdata = []
+    annotations = []
+
+    for node in G.nodes():
+        x, y = G.nodes[node]['pos']
+        node_x.append(x)
+        node_y.append(y)
+        node_color.append('rgba(149, 165, 166, 0.5)' if node not in set(filtered_comm['Sender']) | set(filtered_comm['Receiver']) else 'rgba(52, 152, 219, 1)')
+        node_customdata.append(G.nodes[node]['customdata'])
+
+    # Define hovertemplate
+    hovertemplate = '<br>'.join([f'{col}: %{{customdata[{i}]}}' for i, col in enumerate(df_cells.columns)])
+    
+    node_trace = go.Scatter(
+        x=node_x, y=node_y, mode='markers', hoverinfo='text',
+        marker=dict(color=node_color, size=8),
+        customdata=node_customdata,
+        hovertemplate=hovertemplate
+    )
+
+    # Add edges as annotations
+    for edge in G.edges():
+        x0, y0 = G.nodes[edge[0]]['pos']
+        x1, y1 = G.nodes[edge[1]]['pos']
+        ax, ay, x, y = adjust_arrow_position(x0, y0, x1, y1, node_size=5)
+        annotations.append(
+            dict(ax=ax, ay=ay, axref='x', ayref='y', x=x, y=y, xref='x', yref='y',
+                 arrowwidth=1, arrowcolor="#808080", arrowsize=1, showarrow=True, arrowhead=1)
+        )
+
+    fig = go.Figure(
+        data=[node_trace],
+        layout=go.Layout(
+            title='Directed Graph of Communicating Cells',
+            title_x=0.5,
+            showlegend=False,
+            hovermode='closest',
+            template="simple_white",
+            margin=dict(b=20, l=5, r=5, t=40),
+            xaxis=dict(showgrid=False, zeroline=False, showticklabels=True),
+            yaxis=dict(showgrid=False, zeroline=False, showticklabels=True, autorange='reversed'),
+            annotations=annotations
+        )
+    )
+
     return fig, ""
+
+def adjust_arrow_position(x0, y0, x1, y1, node_size):
+    # Calculate direction of the edge
+    dx = x1 - x0
+    dy = y1 - y0
+    distance = math.hypot(dx, dy)
+    if distance ==0:
+        distance =0.5
+    dx *= node_size / distance
+    dy *= node_size / distance
+
+    # Adjusted positions
+    return x0 + dx, y0 + dy, x1 - dx, y1 - dy
+
 
 if __name__ == '__main__':
     app.run_server(port = 8081, debug=True)
